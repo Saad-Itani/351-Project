28fd7ed28f1c98eb919720ec80b8f0a3f17b774c
leenhariri <118726727+leenhariri@users.noreply.github.com>
2022-11-23 14:57:08 +0200

    Update auth.py

--- Website/auth.py
+++ Website/auth.py
@@ -1,13 +1,32 @@
-from flask import Blueprint 
-
+from flask import Blueprint , render_template, request
+from flask_mysqldb import MySQL
 ##### Similar to views file but this for the authentication (login page...)
-
+#configure db
 auth = Blueprint('auth',__name__) ## attention to naming is unimportant, just by convention/ease 
+auth.config['MYSQL_HOST']='localhost'
+auth.config['MYSQL_USER']='root'
+auth.config['MYSQL_PASSWORD']='leen12345'
+auth.config['MYSQL_DATABASE']='hotel_system'
 
+#instaniate an object for this mysql model
+mysql=MySQL(auth) # we can make use of this mysql object within our post request to make an entry into the database -leen
 #### Making routes for each login, logout , and signup and assigning dummy HTML headers for now* (20.11.2022 , 12:30 am) ~ Saad
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])#identify this URL accepts post and get requests, if it is a GET request, we display the form to the user, if its a POST request we store the details onto the database -leen
 def login():
+    if request.method='POST' #this means the submit button was hit from the form, and now we have the form data which needs to be stored onto the database -leen
+    #fetch form data
+    userDetails= request.form
+    First_Name= userDetails['First_Name'] #stores name
+    Last_Name=userDetails['Last_Name']
+    ID= userDetails['ID']
+    Password=userDetails['Password']
+    Dob =userDetails['Dob']
+    Balance =userDetails['name']
+    Email_address=userDetails['Email_address'] #stores email , we need to store these values in the database to be able to use them - leen
+    cursor = mysql.connection.cursor()
+    cur.execute("INSERT INTO users(ID,First_Name,Last_Name,Password,Dob,Balance) VALUES ()")
+    
     return "<h1>login<h1>"
 
 @auth.route('/logout')
