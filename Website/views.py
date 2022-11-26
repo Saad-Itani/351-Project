79ddf75038fc25c481eb926ef7ed731e7d5fafd7
Saad-Itani <saadallah.itani123@gmail.com>
2022-11-26 08:58:31 +0200

    Saad 26.11.2022

--- Website/views.py
+++ Website/views.py
@@ -8,16 +8,16 @@ views = Blueprint('views',__name__) ## attention to naming is unimportant, just 
 #app.register_blueprint(views, url_prefix='/')
 
 #configuredb
-views.config[MYSQL_HOST]='localhost'
-views.config[MYSQL_USER]='root'
-views.config[MYSQL_PASSWORD]='Leen@123456'
-views.config[MYSQL_DB]='flaskapp'
-mysql=MySQL(views)
+#views.config[MYSQL_HOST]='localhost'
+#views.config[MYSQL_USER]='root'
+#views.config[MYSQL_PASSWORD]='Leen@123456'
+#views.config[MYSQL_DB]='flaskapp'
+#mysql=MySQL(views)
 @views.route('/', methods=['GET', 'POST']) ## GET and POST are Flask 
 
 def home():
     return render_template("index.html")
-    @views.route('/',methods=['GET','POST'])
+''' @views.route('/',methods=['GET','POST'])
     def index()
     if request.method=='POST' #check whether post or get method 
         #fetch form data
@@ -30,9 +30,9 @@ def home():
         cur.execute("INSERT INTO rooms(CheckInDate,CheckOutDate,RoomType,RoomNumber) VALUES(char,char,char,int)",(CheckInDate,CheckOutDate,RoomType,RoomNumber))
         mysql.connection.commit() #saves changes within our database-leen
         cur.close()
-        return render_template("index.html")
-    @views.route('/rooms',methods=['GET','POST'])
-def rooms():
+        return render_template("index.html")'''
+#@views.route('/rooms',methods=['GET','POST'])
+'''def rooms():
     #fetch details from database
     cur=mysql.connection.cursor()
     resultValue= cur.execute("SELECT * FROM rooms")
@@ -40,3 +40,4 @@ def rooms():
         roomDetails=cur.fetchall() # return all rows that have been fetched by mysql cursor -leen
         return render_template ('rooms.html',roomDetails=roomDetails)
     return render_template("rooms.html")
+'''
