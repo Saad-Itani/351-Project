73f7ffce64f532a7c524290ae3bfa8592bbb1ebc
leenhariri <118726727+leenhariri@users.noreply.github.com>
2022-11-29 12:23:31 +0200

    Update views.py

--- Website/views.py
+++ Website/views.py
@@ -17,27 +17,3 @@ views = Blueprint('views',__name__) ## attention to naming is unimportant, just 
 
 def home():
     return render_template("index.html")
-''' @views.route('/',methods=['GET','POST'])
-    def index()
-    if request.method=='POST' #check whether post or get method 
-        #fetch form data
-        roomDetails=request.form
-        CheckInDate=roomDetails['CheckInDate']
-        CheckOutDate=roomDetails['CheckOutDate']
-        RoomType=roomDetails['RoomType']
-        RoomNumber=roomDetails['RoomNumber']
-        cur= mysql.connection.cursor()
-        cur.execute("INSERT INTO rooms(CheckInDate,CheckOutDate,RoomType,RoomNumber) VALUES(char,char,char,int)",(CheckInDate,CheckOutDate,RoomType,RoomNumber))
-        mysql.connection.commit() #saves changes within our database-leen
-        cur.close()
-        return render_template("index.html")'''
-#@views.route('/rooms',methods=['GET','POST'])
-'''def rooms():
-    #fetch details from database
-    cur=mysql.connection.cursor()
-    resultValue= cur.execute("SELECT * FROM rooms")
-    if resultValue>0:
-        roomDetails=cur.fetchall() # return all rows that have been fetched by mysql cursor -leen
-        return render_template ('rooms.html',roomDetails=roomDetails)
-    return render_template("rooms.html")
-'''
