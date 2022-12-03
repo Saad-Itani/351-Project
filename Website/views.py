629f69a418124ea841febe5f2af4a33cb1706324
mymni <84423825+mymni@users.noreply.github.com>
2022-12-03 11:14:32 +0200

    Update views.py

--- Website/views.py
+++ Website/views.py
@@ -2,6 +2,9 @@ from flask import Flask, Blueprint , render_template, render_template, request, 
 from flask_mysqldb import MySQL
 from .auth import mysql
 import MySQLdb.cursors
+from random import *
+from datetime import datetime
+
 
 #from . import app 
 ##### This is for defining a blueprint of our application which means it has  
@@ -23,22 +26,41 @@ def invoice():
 #####################################
 
 @views.route('/reservation', methods = ['GET','POST'])
+
+@views.route('/reservation', methods = ['GET','POST'])
 def reservation():
     if 'loggedin' in session:
         if request.method == "POST" :
-            if "check-in" in request.form:
-                if "check-out" in request.form:
-                    check_in = request.form["check-in"]
-                    check_out = request.form ["check-out"]
-                    Email = session['username']
+            if "checkin-date" in request.form:
+                if "checkout-date" in request.form:
+                    invoice = randint(10000000,99999999)
+                    Email_address = session['username']
+                    check_in = request.form['checkin-date']
+                    check_out = request.form ['checkout-date']
+                    room_num = request.form ['rooms']
+                    room_type = request.form ['rooms']
+                    checkin_date = datetime.datetime.strptime(check_in,'%m-%d-%Y') 
+                    checkin_day = checkin_date.day
+                    checkin_month = checkin_date.month
+                    checkin_year = checkin_date.year
+                    checkout_date = datetime.datetime.strptime(check_out,'%m-%d-%Y') 
+                    checkout_day = checkin_date.day
+                    checkout_month = checkin_date.month
+                    checkout_year = checkin_date.year
+                    days = 365*(checkout_year-checkin_year)+30*(checkout_month-checkin_month)+checkout_day-checkin_day
+                    if days<0:
+                            return render_template("rooms.html")
+                    Balance = 129*days
                     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-                    cur.execute("""SELECT DISTINCT r.room_num, r.price, r.room_type,
-                                    FROM hotel AS h
-                                    LEFT JOIN myroom AS r
-                                        ON h.hotelID = r.hotelID
-                                    ORDER BY r.room_num;""")
-
-    result = cur.execute("SELECT hotel_name, FROM hotel WHERE room_num=%s", [room_num])
+                    cur.execute('INSERT INTO reservation VALUES (%s, % s, % s, % s, %s, %s)', (invoice, Email_address, check_in,check_out,room_num, Balance))
+                    cur.execute(''' FROM user
+                                    LEFT JOIN reservation
+                                        ON user.Balance = reservation.Balance;
+                                    FROM user
+                                    LEFT JOIN myroom
+                                        ON user.Balance = reservation.Balance;
+                                ''')
     cur.close()
-    return render_template("rooms.html")
+    return render_template("index.html")
+
 
